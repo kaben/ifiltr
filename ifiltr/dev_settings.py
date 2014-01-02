bd531c0c23c711814627d3914cec2978c5515037
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2014-01-02 04:33:16 +0000

    Apply local, throwaway development db password.

--- ifiltr/dev_settings.py
+++ ifiltr/dev_settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ifiltr',                      # Or path to database file if using sqlite3.
         'USER': 'ifiltr',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'PASSWORD': 'mer6&clocking',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
